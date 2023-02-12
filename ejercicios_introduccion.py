71d95bc14564ce63e95a3b371b846b1d34243bbd
Enrique Kessler Martínez <enrikes@amazon.com>
2023-02-12 12:38:49 +0100

    Add refactored ejercicios_introduccion.py and tests

--- ejercicios_introduccion.py
+++ ejercicios_introduccion.py
@@ -1,114 +1,102 @@
-#Ejercicio 1
-print('\n********* EJERCICIO 1 *********')
-numero=input('Introduzca entero: ')
-n=int(numero)
-total=0
-for i in range(n):
-    division=((-1)**i)/((2*i)+1)
-    formula=4*division
-    total=total+formula
-    
-print(f'La suma de los primeros {n} términos de la secuencia es: {total}')
+# Ejercicio 1
+def suma_n_primeros(n):
+    total = 0
+    for i in range(n):
+        division = ((-1) ** i) / ((2 * i) + 1)
+        formula = 4 * division
+        total = total + formula
+    return total
 
 
-#Ejercicio 2
-print('\n********* EJERCICIO 2 *********')
-numero=input('Introduzca entero: ')
-n=int(numero)
-
-print(f'\nLa tabla del {n} es: ')
-for i in range(11):
-    mult=n*i
-    print(mult)
+# Ejercicio 2
+def tabla_del_n(numero, top):
+    return [numero * i for i in range(top)]
 
 
+# Ejercicio 3
+# Hard to test with whitespace.
 
-#Ejercicio 3
-print('\n********* EJERCICIO 3 *********')
-numero=input('Introduzca numero de filas: ')
-n=int(numero)
-for i in range(n+1):
-    for j in range(i):
-        print(j+1, end=' ')
-        if(j==i-1):
-            print(' ')
+# Ejercicio 4
+# No termino de entender la diferencia entre 4 y 5 solo viendo el código +
+# code con break sucks.
 
 
-#Ejercicio 4
-print('\n********* EJERCICIO 4 *********')
-numero=input('Introduzca entero: ')
-num=int(numero)
-if num<=0:
-    print("Ha introducido un número negativo, introduzca uno positivo.")
-
-if num==1:
-    print("El número 1 no es un número primo")
-if num==2:
-    print("El número 2 es un número primo")
-else:
-    for i in range(2, num):
-        if num % i == 0:
-            print(f"El número {num} no es primo")
-            break
-    if i==num-1:        ##Hago esto porque si i=num-1 significa que ha recorrido el bucle anterior sin encontrar ningún resto=0. En caso de no poner el if, mi programa solo detectaría números no primos divisibles por 2.
-        print(f"El número {num} es primo")
-            
-    
-
-
-
-#Ejercicio 5
-print('\n********* EJERCICIO 5 *********')
+# Ejercicio 5
 def es_primo(numero):
-    if numero<2:
+    if numero < 2:
         return False
     for i in range(2, numero):
         if numero % i == 0:
             return False
-            
-        
+
     return True
-        
-
-print(es_primo(11))
-print(es_primo(9))
-print(es_primo(2))
 
 
-##Ejercicio 6
-print('\n********* EJERCICIO 6 *********')
-numero=input('Introduzca entero: ')
-num=int(numero)
-if num<=0:
-    print("Ha introducido un número negativo, introduzca uno positivo.")
+# Ejercicio 6
+def primos_inferiores(numero):
+    if numero <= 0:
+        print("Ha introducido un número negativo, introduzca uno positivo.")
+        return []
+    if numero == 1 or numero == 2:
+        print(f"No existen números primos inferiores a {numero}")
+        return []
 
-if num==1 | num==2:
-    print(f"No existen números primos inferiores a {num}")
-else:
+    return [i for i in range(2, numero) if es_primo(i)]
 
-    print(f"Los números primos inferiores a {num} son: ")
-    for i in range(2,num):
-        if es_primo(i):
-         print(i, end=' ' )
-    
 
-#Ejercicio 7
-print('\n\n********* EJERCICIO 7 *********')
-personas={
-    'pedro':28,
-    'maría':21,
-    'marta':22
-}
+# Ejercicio 7
+def esperanza_de_vida(personas, esperanza_por_edad):
+    return {
+        nombre: edad + esperanza_por_edad[edad] for nombre, edad in personas.items()
+    }
 
-esperanza_adicional={
-    28:53.4,
-    21:65.6,
-    22:64.5
-}
 
-personas_esperanza={
-    nombre: edad + esperanza_adicional[edad] for nombre, edad in personas.items()
-}
-print(f"La esperanza de vida de Pedro es de {personas_esperanza['pedro']} años.")
-print(f"La esperanza de vida de María es de {personas_esperanza['maría']} años.")
-print(f"La esperanza de vida de Marta es de {personas_esperanza['marta']} años.")
+def main():
+    print("\n********* EJERCICIO 1 *********")
+    numero = input("Introduzca entero: ")
+    n = int(numero)
+    total = suma_n_primeros(n)
+    print(f"La suma de los primeros {n} términos de la secuencia es: {total}")
+
+    print("\n********* EJERCICIO 2 *********")
+    numero = input("Introduzca entero: ")
+    n = int(numero)
+    print(f"\nLa tabla del {n} es: {tabla_del_n(numero, 11)}")
+
+    print("\n********* EJERCICIO 3 *********")
+    numero = input("Introduzca numero de filas: ")
+    n = int(numero)
+    for i in range(n + 1):
+        for j in range(i):
+            print(j + 1, end=" ")
+            if j == i - 1:
+                print(" ")
+
+    print("\n********* EJERCICIO 4 *********")
+    numero = input("Introduzca entero: ")
+    numero = int(numero)
+    print(f"El número {numero} {'es' if es_primo(numero) else 'no es'} primo")
+
+    print("\n********* EJERCICIO 5 *********")
+    print(es_primo(11))
+    print(es_primo(9))
+    print(es_primo(2))
+
+    print("\n********* EJERCICIO 6 *********")
+    numero = input("Introduzca entero: ")
+    num = int(numero)
+    primos = primos_inferiores(num)
+    print(f"Primos inferiores a {num}: {primos}")
+
+    print("\n\n********* EJERCICIO 7 *********")
+    PERSONAS = {"pedro": 28, "maría": 21, "marta": 22}
+    ESPERANZA = {28: 53.4, 21: 65.6, 22: 64.5}
+
+    edv = esperanza_de_vida(PERSONAS, ESPERANZA)
+    print(f"La esperanza de vida de Pedro es de {edv['pedro']} años.")
+    print(f"La esperanza de vida de María es de {edv['maría']} años.")
+    print(f"La esperanza de vida de Marta es de {edv['marta']} años.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_ejercicios_introduccion.py
@@ -0,0 +1,68 @@
+from ejercicios_introduccion import (
+    es_primo,
+    suma_n_primeros,
+    tabla_del_n,
+    primos_inferiores,
+    esperanza_de_vida,
+)
+
+
+def test_suma_n_primeros():
+    expected = [0, 4.0, 2.666666666666667, 3.466666666666667]
+    found = [suma_n_primeros(num) for num in range(4)]
+
+    assert expected == found
+
+
+def test_tabla_del_n():
+    tests = [
+        (1, 1),
+        (1, 2),
+        (2, 6),
+        (-1, 3),
+        (-3, 4),
+        (0, 6),
+    ]
+    expected = [
+        [0],
+        [0, 1],
+        [0, 2, 4, 6, 8, 10],
+        [0, -1, -2],
+        [0, -3, -6, -9],
+        [0, 0, 0, 0, 0, 0],
+    ]
+    found = [tabla_del_n(numero, top) for numero, top in tests]
+
+    assert expected == found
+
+
+def test_es_primo():
+    tests = [0, 1, 2, 3, 5, 6, 9, 13]
+    expected = [False, False, True, True, True, False, False, True]
+    found = [es_primo(num) for num in tests]
+
+    assert expected == found
+
+
+def test_primos_inferiores():
+    tests = [-1, 0, 1, 2, 5, 9]
+    expected = [[], [], [], [], [2, 3], [2, 3, 5, 7]]
+    found = [primos_inferiores(num) for num in tests]
+
+    assert expected == found
+
+
+def test_esperanza_de_vida():
+    tests = [
+        ({"a": 1, "b": 2, "c": 3}, {1: 50, 2: 65, 3: 75}),
+        ({"a": 1, "b": 1, "c": 3}, {1: 50, 2: 65, 3: 75}),
+        ({}, {}),
+    ]
+    expected = [
+        {"a": 51, "b": 67, "c": 78},
+        {"a": 51, "b": 51, "c": 78},
+        {},
+    ]
+    found = [esperanza_de_vida(p, e) for p, e in tests]
+
+    assert expected == found
