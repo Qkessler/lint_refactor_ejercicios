05d20577517af715c651d33b88b3f7852c5dbccb
Enrique Kessler Martínez <enrikes@amazon.com>
2023-02-12 13:01:02 +0100

    Add README and exercise template with the same tests

---
+++ ejer_template.py
@@ -0,0 +1,14 @@
+def suma_n_primeros(n):
+    return 0
+
+def tabla_del_n(numero, top):
+    return []
+
+def es_primo(numero):
+    return False
+
+def primos_inferiores(numero):
+    return []
+
+def esperanza_de_vida(personas, esperanza_por_edad):
+    return {}
